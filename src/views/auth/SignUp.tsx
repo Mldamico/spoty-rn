d8243bba16cc7ca47ccd7556f43b87121dc0fcaf
mldamico <matias.lioneldamico@gmail.com>
2023-11-20 12:21:36 -0300

    Onchange setup in input

--- src/views/auth/SignUp.tsx
+++ src/views/auth/SignUp.tsx
@@ -1,9 +1,14 @@
-import {SafeAreaView, StyleSheet, View} from 'react-native';
-import React from 'react';
+import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
 import colors from '@utils/colors';
 import AuthInputField from '@components/auth/AuthInputField';
 
 const SignUp = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.formContainer}>
@@ -11,20 +16,36 @@ const SignUp = () => {
           placeholder="Your Name"
           label="Name"
           containerStyle={styles.marginBottom}
+          onChange={text => {
+            setUserInfo({...userInfo, name: text});
+          }}
         />
         <AuthInputField
           placeholder="Your Email"
           label="Email"
           keyboardType="email-address"
           containerStyle={styles.marginBottom}
+          autoCapitalize="none"
+          onChange={text => {
+            setUserInfo({...userInfo, email: text});
+          }}
         />
         <AuthInputField
           placeholder="Your Password"
           label="Password"
           autoCapitalize="none"
           secureTextEntry
+          onChange={text => {
+            setUserInfo({...userInfo, password: text});
+          }}
         />
       </View>
+      <Button
+        title="Sign Up"
+        onPress={() => {
+          console.log(userInfo);
+        }}
+      />
     </SafeAreaView>
   );
 };
