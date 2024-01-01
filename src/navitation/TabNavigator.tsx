f55be5914239b362f5447699c9f61113100de5b4
matias damico <matiasdamico@MacBook-Pro-de-matias.local>
2024-01-01 11:29:09 -0300

    Tab theme

--- src/navitation/TabNavigator.tsx
+++ src/navitation/TabNavigator.tsx
@@ -3,14 +3,49 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '@views/Home';
 import Profile from '@views/Profile';
 import Upload from '@views/Upload';
+import colors from '@utils/colors';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialComIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={Home} />
-      <Tab.Screen name="ProfileScreen" component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: colors.PRIMARY,
+        },
+      }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={Home}
+        options={{
+          tabBarIcon: props => {
+            return (
+              <AntDesign name="home" size={props.size} color={props.color} />
+            );
+          },
+          tabBarLabel: 'Home',
+        }}
+      />
+      <Tab.Screen
+        name="ProfileScreen"
+        component={Profile}
+        options={{
+          tabBarIcon: props => {
+            return (
+              <MaterialComIcon
+                name="account-music-outline"
+                size={props.size}
+                color={props.color}
+              />
+            );
+          },
+          tabBarLabel: 'Upload',
+        }}
+      />
       <Tab.Screen name="UploadScreen" component={Upload} />
     </Tab.Navigator>
   );
--- src/navitation/index.tsx
+++ src/navitation/index.tsx
@@ -1,17 +1,82 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import AuthNavigator from './AuthNavigator';
-import {useSelector} from 'react-redux';
-import {getAuthState} from 'src/store/auth';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  getAuthState,
+  updateBusyState,
+  updateLoggedInState,
+  updateProfile,
+} from 'src/store/auth';
 import TabNavigator from './TabNavigator';
+import {Keys, getFromAsyncStorage} from '@utils/asyncStorage';
+import client from 'src/api/client';
+import {StyleSheet, View} from 'react-native';
+import Loader from '@ui/Loader';
+import colors from '@utils/colors';
+
+const appTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: colors.PRIMARY,
+    primary: colors.CONTRAST,
+  },
+};
 
 const AppNavigator = () => {
-  const {loggedIn} = useSelector(getAuthState);
+  const {loggedIn, busy} = useSelector(getAuthState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchAuthInfo = async () => {
+      dispatch(updateBusyState(true));
+      try {
+        const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+        if (!token) {
+          dispatch(updateBusyState(false));
+          return;
+        }
+        const {data} = await client.get('/auth/is-auth', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log(data);
+        dispatch(updateProfile(data.profile));
+        dispatch(updateLoggedInState(true));
+      } catch (error) {
+        console.log('error ' + error);
+      }
+      dispatch(updateBusyState(false));
+    };
+    fetchAuthInfo();
+  }, [dispatch]);
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={appTheme}>
+      {busy ? (
+        <View style={{...StyleSheet.absoluteFillObject, ...styles.container}}>
+          <Loader />
+        </View>
+      ) : null}
       {loggedIn ? <TabNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 };
 
 export default AppNavigator;
+
+const styles = StyleSheet.create({
+  container: {
+    // position: 'absolute',
+    // top: 0,
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    backgroundColor: colors.OVERLAY,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+  },
+});
